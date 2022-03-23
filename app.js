66f1604e0d9b90dd44df2c922e5c93f87e781857
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-03-23 08:56:55 +0530

    controllers added

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const placesRoutes = require("./routes/places-routes")
+
 
 const app = express()
+
+app.use('/api/places' , placesRoutes)
+
+app.use((err, req, res, next) => {
+    if(res.headerSent) {
+        return next(err)
+    }
+    res.status(err.code || 500)
+    res.json({message: err.message || "An unknown error occured."})
+})
 
 app.listen(5000 , () => {
     console.log("app is listening on port 5000")
