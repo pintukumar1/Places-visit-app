041792f78bebab662e8fd80e56c35439bda5e58c
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-03-26 16:46:56 +0530

    deployig..

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
@@ -28,10 +26,6 @@ app.use((req, res, next) => {
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", 'index.html'))
-})
 
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route.', 404);
